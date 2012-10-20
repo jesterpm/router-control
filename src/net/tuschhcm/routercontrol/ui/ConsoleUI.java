cf6f139fed865fb0ae71e760104a587a9ef2cdaf
Craig Tuschhoff <tuschhcm@gmail.com>
2012-10-20 00:41:16 -0700

    text formatting changes

--- src/net/tuschhcm/routercontrol/ui/ConsoleUI.java
+++ src/net/tuschhcm/routercontrol/ui/ConsoleUI.java
@@ -35,7 +35,7 @@ public class ConsoleUI implements UserInterface {
     	String userInput;
     	boolean running = true;
     	
-    	System.out.println("Welcome to Router Control!");
+    	System.out.println("/n/nWelcome to Router Control!");
         System.out.println("--------------------------");
         System.out.println("Available commands: \nLock: Locks/unlocks the physical controls " +
         		"on the router\nExit: Exits the application");
